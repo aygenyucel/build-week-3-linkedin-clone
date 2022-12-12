155e3156a1de7310a1feafb6bd7f6d4ab464cc29
Simone Backes <88430766+sihbackes@users.noreply.github.com>
2022-12-12 16:34:48 +0000

    main page style

--- src/components/profilePage/ProfilePage.jsx
+++ src/components/profilePage/ProfilePage.jsx
@@ -1,15 +1,22 @@
-import { Col, Container, Row } from "react-bootstrap";
 import ProfileMainCard from "../profileMainCard/ProfileMainCard";
+import SideBar from "../sidebar/SideBar";
+import { Container } from "react-bootstrap";
+import "./profilePage.css";
 
 const ProfilePage = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
+    <>
+      <Container className="d-flex mt-2">
+        <div className="main-card-div">
           <ProfileMainCard />
-        </Col>
-      </Row>
-    </Container>
+        </div>
+
+        <div className="sidebar-div">
+          <SideBar />
+        </div>
+      </Container>
+    </>
   );
 };
+
 export default ProfilePage;
