11ac43238cacda071579996c761a2d3dfb792da0
Raulanthropos <ipsichias@yandex.com>
2022-12-12 17:32:12 +0200

    sidebar is mostly complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import SideBar from "./components/sidebar/SideBar";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <SideBar />
+  </div>;
 }
 
 export default App;
