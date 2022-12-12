b1af434fd2a9c479a37f369b26f4eef5d6cebd8d
Raulanthropos <ipsichias@yandex.com>
2022-12-12 14:59:01 +0200

    redux works, to be used promptly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import TestComponent from "./components/TestComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <TestComponent />
+  </div>;
 }
 
 export default App;
---
+++ src/redux/actions/index.js
@@ -0,0 +1,31 @@
+export const GET_PROFILE = "GET_PROFILE";
+
+export const getProfileAction = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzk2ZjNmYmM5NmRmYjAwMTUyMWE1YmUiLCJpYXQiOjE2NzA4MzcyNDQsImV4cCI6MTY3MjA0Njg0NH0.lj6PsFRCQqFIpT6qYY681bm60-LvcXLTb-HKHJoptLI",
+  },
+}
+  return async (dispatch, getState) => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/me`,
+        options
+      );
+      if (response.ok) {
+        const profileData = await response.json();
+        console.log("Get user profile", profileData);
+        dispatch({
+          type: GET_PROFILE,
+          payload: profileData,
+        });
+      } else {
+        console.log("Error fetching data");
+      }
+    } catch (error) {
+      console.log("🚀 error", error);
+    }
+  };
+};
