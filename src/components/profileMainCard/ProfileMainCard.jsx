4a0c80ee923339e6410e0e7e63b28695dd3af1e8
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-12-12 18:38:53 +0300

    fixed the template, added styling,

--- src/components/profileMainCard/ProfileMainCard.jsx
+++ src/components/profileMainCard/ProfileMainCard.jsx
@@ -1,17 +1,18 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { FiEdit2 } from "react-icons/fi";
+import "./profileMainCard.css";
 
 const ProfileMainCard = () => {
   return (
-    <Container className="profile-main-card" fluid>
+    <div className="profile-main-card mb-4">
       <div className="profile-main-card-background">
         <img
           src="assets/profile-background-default.png"
           alt="profile background"
         />
       </div>
-      <Container className="profile-main-card-middle">
-        <div className="d-flex justify-content-between">
+      <Container className="profile-main-card-middle mb-4">
+        <div className="d-flex justify-content-between align-items-end">
           <div className="profile-img">
             <img src="assets/profile-img-default.jpeg" alt="profile img" />
           </div>
@@ -25,19 +26,21 @@ const ProfileMainCard = () => {
           <Col xs={8}>
             <div className="main-info d-flex flex-column">
               <div className="profile-name">Michael Scott</div>
-              <div className="profile-about">
+              <div className="profile-about mb-2">
                 Regional Manager at Dundler Mifflin
               </div>
-              <div className="profile-details d-flex">
+              <div className="profile-details d-flex align-items-center mb-2">
                 <div className="profile-location mr-2">
                   Roanoke, Texas, United States
                 </div>
-                <div className="profile-contact-info-btn">
+                <div className="profile-contact-info-btn d-flex align-items-center">
                   <a href="/">Contact info</a>
                 </div>
               </div>
-              <div className="profile-connections">
-                <span className="connections-number">96</span> Connections
+              <div className="profile-connections mb-2">
+                <a href="/">
+                  <span className="connections-number">96</span> Connections{" "}
+                </a>
               </div>
               <div className="profile-buttons d-flex">
                 <div className="mr-2">
@@ -54,16 +57,18 @@ const ProfileMainCard = () => {
               </div>
             </div>
           </Col>
-          <Col xs={4} className="d-flex">
-            <div className="company-logo">
-              <img src="assets/company-logo-default.png" alt="company-logo" />
+          <Col xs={4}>
+            <div className="company-info d-flex align-items-center">
+              <div className="company-logo mr-2">
+                <img src="assets/company-logo-default.png" alt="company-logo" />
+              </div>
+              <div className="company-name d-flex ">Dundler Mifflin</div>
             </div>
-            <div className="company-name">Dundler Mifflin</div>
           </Col>
         </Row>
         <Row></Row>
       </Container>
-    </Container>
+    </div>
   );
 };
 
