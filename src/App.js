cd1fd105ad1599d2e9225901fc55936ab6601e65
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-12-13 16:46:30 +0300

    component for users profile&displaying experiences

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/footer/footer";
 import NavBar from "./components/NavBar/NabVabarComponent";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserProfilePage from "./components/userProfilePage/UserProfilePage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<ProfilePage />} />
+          <Route path="/:id" element={<UserProfilePage />} />
           {/* <ProfilePage /> */}
         </Routes>
         <Footer />
