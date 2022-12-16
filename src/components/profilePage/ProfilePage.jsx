2bdfccef9250c78f82c3be2456e2525df0ab7f7c
Raulanthropos <ipsichias@yandex.com>
2022-12-16 16:46:57 +0200

    Update ProfilePage.jsx

--- src/components/profilePage/ProfilePage.jsx
+++ src/components/profilePage/ProfilePage.jsx
@@ -15,7 +15,7 @@ import AboutCard from "../aboutCard/AboutCard";
 import { useLocation, useParams } from "react-router-dom";
 import SuggestedCard from "../suggestedCard/SuggestedCard";
 import ActivityCard from "../activityCard/ActvityCard";
-import ResourcesCard from "../resourcesCard/ResourcesCard";
+import ResourcesCard from "../ResourcesCard/ResourcesCard";
 import EducationCard from "../educationCard/EducationCard";
 import LanguagesCard from "../languagesCard/LanguagesCard";
 import Footer from "../footer/footer";
