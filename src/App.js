5b634a00cbcf1970ba42faa8ebf005c00581a1de
Raulanthropos <ipsichias@yandex.com>
2022-12-16 10:27:05 +0200

    Ready to merge

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/footer/footer";
 import NavBar from "./components/NavBar/NabVabarComponent";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FeedPage from "./components/FeedPageFiles/FeedPage/FeedMainPage";
 
 function App() {
   return (
